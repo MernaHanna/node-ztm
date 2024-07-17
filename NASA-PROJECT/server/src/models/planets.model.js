4aceda6260883d7de9181ea309b83531a786db67
MernaHanna <merna.m.hanna@gmail.com>
2024-07-17 19:12:02 +0300

    aa

--- NASA-PROJECT/server/src/models/planets.model.js
+++ NASA-PROJECT/server/src/models/planets.model.js
@@ -1,3 +1,57 @@
-const planets = [];
+// const planets = [];
 
-module.exports = planets;
+// module.exports = planets;
+
+const parse = require('csv-parse');
+const fs = require('fs');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
+/* 
+  const promise = new Promise( (resolve, reject) => {
+    resolve(42);
+  });
+  promise.then((result) => {
+  });
+  const result = await promise;
+  console.log(result);
+*/
+
+function loadPlanetsData() {
+  fs.createReadStream('../../data/kepler_data.csv')
+    .pipe(
+      parse({
+        comment: '#', // treat lines that start with this character as comment
+        columns: true, // return each object in csv file as a js object with (key: value) pairs rather than just as an array of values
+      })
+    )
+    .on('data', (data) => {
+      if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+      }
+    })
+    .on('error', (err) => {
+      console.log(err);
+    })
+    .on('end', () => {
+      console.log(
+        habitablePlanets.map((planet) => {
+          return planet['kepler_name'];
+        })
+      );
+      console.log(`${habitablePlanets.length} habitable planets found!`);
+    });
+}
+
+module.exports = {
+  planets: habitablePlanets,
+};
