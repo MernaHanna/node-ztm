cc23ae918acd519a9c76cbcce8dbdcddc0330b8b
MernaHanna <merna.m.hanna@gmail.com>
2024-07-24 23:34:57 +0300

    117

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -8,10 +8,19 @@ const app = express();
 
 // app.listen();
 // to whitelist multiple origins => see the npm package documentation
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public'))); //to serve our static built files in public folder
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html')); // to route to the frontend without including the port in the url => to view launch page by default
+});
 
 module.exports = app;
