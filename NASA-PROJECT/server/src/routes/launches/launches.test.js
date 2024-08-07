c14814efea3f409172b0b0598ab222dcbdcb0098
MernaHanna <merna.m.hanna@gmail.com>
2024-08-07 17:53:36 +0300

    134

--- NASA-PROJECT/server/src/routes/launches/launches.test.js
+++ NASA-PROJECT/server/src/routes/launches/launches.test.js
@@ -1,8 +1,18 @@
+const request = require('supertest');
+const app = require('../../app');
+
 // create test fixture with different test cases
 describe('Test GET /launches', () => {
-  test('It should respond with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  // async await approach is much better than the callback promise approach
+  test('It should respond with 200 success', async () => {
+    // supertest requires the app variable (express app defined in app.js)
+    // const response = await request(app).get('/launches');
+    // expect(response.statusCode).toBe(200);
+    // when testing http requests we may use supertest chaining method expect
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
   });
 });
 
