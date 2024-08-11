7fcfb000856c135bb8ec2e166ccdf8252c82ee7a
MernaHanna <merna.m.hanna@gmail.com>
2024-08-11 18:29:37 +0300

    137

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// naive function
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    // event loop is blocked...
+  }
+}
+
 app.get('/', (req, res) => {
   res.send('Performance example');
 });
 
 app.get('/timer', (req, res) => {
-  //
+  //delay the response
+  delay(9000); // the server can't process any other code while delay is processing
+  res.send('Ding ding ding!');
 });
+
+app.listen(3000);
