4e7bef23128dd44945043a9cf4843eb7b9196587
MernaHanna <merna.m.hanna@gmail.com>
2024-08-12 18:26:42 +0300

    137

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+// cluster is a built in node module
+const cluster = require('cluster');
+
+// for clustering to work in windows
+// doesn't need to be included if on mac or linux
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
 const app = express();
 
@@ -11,13 +17,30 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  // pid get the id of the current process running
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   //delay the response
   delay(9000); // the server can't process any other code while delay is processing
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(3000);
+// server.js will run with the master process as well as the worker processes.. the only difference is the isMaster condition
+console.log('Running server.js...');
+
+// to differentiate between master and worker use the boolean flag isMaster
+if (cluster.isMaster) {
+  console.log('Master has been started...');
+  // fork creates a worker
+  // we can call the fork function however many times we like
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('Worker has been started...');
+  // it doesn't matter wheather we run our application using node barebones or express.. node cluster module understands the listen function
+  // we only want workers to listen to http requests and respond to them
+  // all workers will listen on port 3000.. node http server will divide incoming requests between workers
+  app.listen(3000);
+}
